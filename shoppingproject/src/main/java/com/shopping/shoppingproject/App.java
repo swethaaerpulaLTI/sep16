706df25d332a9f893adfbb6883ba3cc5fc181406
swethaaerpulaLTI <Swetha.Aerpula@lntinfotech.com>
2021-09-16 10:12:43 +0530

    pppp

---
+++ shoppingproject/src/main/java/com/shopping/shoppingproject/App.java
@@ -0,0 +1,13 @@
+package com.shopping.shoppingproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
